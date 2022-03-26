dcae19295f60d8e73c410661f46a80d65da7d9fc
rf-ferreira <r4fael.s0uza@gmail.com>
2022-03-26 16:47:39 -0300

    add login page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <title>Portfolio Generator</title>
     <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
         button {
             border: none;
             border-radius: 2px;
@@ -29,11 +23,31 @@
             cursor: pointer;
             background-color: #2EA043;
         }
+
+        form {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            height: calc(100vh - 50px);
+            background-color: rgb(136, 136, 136);
+        }
+
+        form p {
+            text-align: center;
+            padding-bottom: 5px;
+        }
+
+        strong {
+            cursor: pointer;
+        }
     </style>
 </head>
 <body>
+    <nav></nav>
     <form action="{{ route('redirectToProvider', 'github') }}">
     @csrf
+    <p>Generates a <strong>portfolio page</strong> using your github repositories as your projects.</p>
     <button><i class="fa-brands fa-github"></i> Login with GitHub</button>
     </form>
 </body>
